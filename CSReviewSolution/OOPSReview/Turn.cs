63b42e6a1c5b263c0949c99754ed0ba3d4120e56
abucog2nait <46579732+abucog2nait@users.noreply.github.com>
2019-01-17 16:44:01 -0700

    Coded Main class, and Turn class

--- CSReviewSolution/OOPSReview/Turn.cs
+++ CSReviewSolution/OOPSReview/Turn.cs
@@ -6,21 +6,38 @@ using System.Threading.Tasks;
 
 namespace OOPSReview
 {
-    class Turn
+    public class Turn
     {
-        public static void Play()
+        //public static void Play()
+        //{
+        //MINE 
+        //Die PlayerOneDie = new Die();
+        //Die PlayerTwoDie = new Die();
+
+        //int playerOne, playerTwo;
+
+        //playerOne = PlayerOneDie.FaceValue;
+        //playerTwo = PlayerTwoDie.FaceValue;
+
+        //Console.WriteLine("Player one rolled a {0}, Player two rolled {1}.", playerOne, playerTwo);
+
+        //TEACHERS******
+
+        //}
+
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+        //optionally, for practice only add constructors
+
+        public Turn()
         {
-            Die PlayerOneDie = new Die();
-            Die PlayerTwoDie = new Die();
 
-            int playerOne, playerTwo;
-
-            playerOne = PlayerOneDie.FaceValue;
-            playerTwo = PlayerTwoDie.FaceValue;
-
-            Console.WriteLine("Player one rolled a {0}, Player two rolled {1}.", playerOne, playerTwo);
         }
-        
 
+        public Turn(int player1, int player2)
+        {
+            Player1 = player1;
+            Player2 = player2; 
+        }
     }
 }
